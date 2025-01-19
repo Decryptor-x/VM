d3f591ae727df021754c5c0f3ab7990522f96160
Decryptor-x <josecmanxd551@gmail.com>
2025-01-19 12:06:00 +0200

    docs: update installation instructions

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
 // Feature addition - 2025-1-5 14:37:00
 // Feature addition - 2025-1-28 10:45:00
 // Feature addition - 2025-1-26 14:56:00
+// Feature addition - 2025-1-19 12:6:00
--- src/vm_manager.sh
+++ src/vm_manager.sh
@@ -3,3 +3,4 @@ echo "VM Manager - Placeholder script"
 # Update Tue Oct  7 12:19:55 EDT 2025
 # Update Tue Oct  7 12:19:56 EDT 2025
 # Update Tue Oct  7 12:19:56 EDT 2025
+# Update Tue Oct  7 12:19:57 EDT 2025
