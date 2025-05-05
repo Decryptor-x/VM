afc2486f406389f030e045ce9a36464475e9cb21
Decryptor-x <josecmanxd551@gmail.com>
2025-05-05 11:13:00 +0300

    docs: add troubleshooting guide

--- src/utils.js
+++ src/utils.js
@@ -84,3 +84,4 @@
 // Feature addition - 2025-5-13 9:55:00
 // Feature addition - 2025-5-22 14:9:00
 // Feature addition - 2025-5-16 9:11:00
+// Feature addition - 2025-5-5 11:13:00
--- src/vm_manager.sh
+++ src/vm_manager.sh
@@ -86,3 +86,4 @@ echo "VM Manager - Placeholder script"
 # Update Tue Oct  7 12:20:27 EDT 2025
 # Update Tue Oct  7 12:20:27 EDT 2025
 # Update Tue Oct  7 12:20:27 EDT 2025
+# Update Tue Oct  7 12:20:28 EDT 2025
